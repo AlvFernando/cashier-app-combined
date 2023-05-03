fd8c0bd1e6371e9208330b0d54dc7a70f9b79bc2
AlvFernando <alvinfernando29@yahoo.com>
2023-05-03 09:13:06 +0700

    adjust spaceHelper

--- src/main/java/com/cashier/app/cashierApp/Helper/SpaceHelper.java
+++ src/main/java/com/cashier/app/cashierApp/Helper/SpaceHelper.java
@@ -2,9 +2,9 @@ package com.cashier.app.cashierApp.Helper;
 
 public class SpaceHelper {
     public static String spaceHelper(String itemName,Integer quantity, Integer price){
-        String initialValue = itemName+quantity+price;
+        //String initialValue = itemName+quantity+price;
         //Integer spacesLeft = 32-initialValue.length()-9+4;
-        Integer spacesLeft = 23-initialValue.length()-String.valueOf(quantity).length() + 4;
+        Integer spacesLeft = 23 - itemName.length() - String.valueOf(quantity).length() + 4;
         //Integer spacesRight = 9 - String.valueOf(price).length();
         Integer spacesRight = 32 - 23 - String.valueOf(price).length();
         String spacesStringLeft = "";
