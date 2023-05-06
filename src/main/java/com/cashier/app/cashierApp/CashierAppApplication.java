a25c880809122e7b5fc5431c4723e7f6df54dfcc
AlvFernando <alvinfernando29@yahoo.com>
2023-05-06 13:04:50 +0700

    add auto start and exit route

--- src/main/java/com/cashier/app/cashierApp/CashierAppApplication.java
+++ src/main/java/com/cashier/app/cashierApp/CashierAppApplication.java
@@ -1,5 +1,10 @@
 package com.cashier.app.cashierApp;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +12,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashierAppApplication {
 
 	public static void main(String[] args) {
+		openHomePage();
 		SpringApplication.run(CashierAppApplication.class, args);
+		
 	}
 
+    private static void openHomePage() {
+        try {
+            URI homepage = new URI("http://localhost:8080/");
+            Desktop.getDesktop().browse(homepage);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
